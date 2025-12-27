6508996691c692d6e700bb96b69e74beca60c79d
tarikcherradi1 <admin@lovemaroc.org>
2025-12-27 22:32:40 +0100

    Refactor React root rendering to init function
    Refactor rendering logic to use an init function that waits for the DOM to be ready.

--- index.tsx
+++ index.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+const init = () => {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+};
+
+// On attend que le DOM soit prêt pour lancer React
+if (document.readyState === 'complete') {
+  init();
+} else {
+  window.addEventListener('load', init);
 }
