5521cd74f98fac690bca39fa342bec319704f967
tarikcherradi1 <admin@lovemaroc.org>
2025-12-27 23:34:05 +0100

    Simplify app rendering logic in index.tsx

--- index.tsx
+++ index.tsx
@@ -2,20 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-const renderApp = () => {
-  const rootElement = document.getElementById('root');
-  if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-  }
-};
-
-if (document.readyState === 'complete') {
-  renderApp();
-} else {
-  window.addEventListener('load', renderApp);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
